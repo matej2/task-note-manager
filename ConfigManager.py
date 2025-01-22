f3925bf9733c36ad8975e1b3d4b89665bf4d7327
matej2 <11059438+matej2@users.noreply.github.com>
2025-01-22 14:59:20 +0100

    Added instructions

--- ConfigManager.py
+++ ConfigManager.py
@@ -3,4 +3,5 @@ class ConfigManager:
         self.font_family = "Arial"
         self.font_size_small = 9
         self.font_size_normal = 12
+        self.font_size_large = 16
         self.date_format = "%d %b, %Y"
--- UI.py
+++ UI.py
@@ -16,6 +16,8 @@ class UI(tkinter.Frame):
         self.open_file = tkinter.Button(self.button_container)
         self.list_button = tkinter.Button(self.button_container)
 
+        self.instruction_container = tkinter.Frame(self.root)
+
         self.task_list = tkinter.Label(self.root)
 
         self.things_done = tkinter.Text(self.root)
@@ -25,12 +27,13 @@ class UI(tkinter.Frame):
         self.configure_button_widgets()
         self.configure_input_widgets()
         self.configure_status_widgets()
+        self.configure_instructions()
 
     def configure_font(self, widget: tkinter.Widget):
         widget.config(font=(self.config_manager.font_family, self.config_manager.font_size_normal))
 
     def configure_button_widgets(self):
-        self.button_container.grid(row=3, column=0, columnspan=2)
+        self.button_container.grid(row=4, column=0, columnspan=2)
 
         self.submit_button.config(text="Submit")
         self.submit_button.grid(row=0, column=0, padx=30)
@@ -41,22 +44,34 @@ class UI(tkinter.Frame):
         self.configure_font(self.open_file)
 
     def configure_input_widgets(self):
-        tkinter.Label(self.root, text="What was done: ").grid(row=0, column=0)
+        tkinter.Label(self.root, text="What was done: ").grid(row=1, column=0)
         self.configure_font(self.things_done)
-        self.things_done.grid(row=0, column=1)
+        self.things_done.grid(row=1, column=1)
         self.things_done.config(height=4, width=20)
         self.things_done.focus_set()
 
-        tkinter.Label(self.root, text="What needs to be done: ").grid(row=1, column=0)
+        tkinter.Label(self.root, text="What needs to be done: ").grid(row=2, column=0)
         self.configure_font(self.things_in_progress)
         self.things_in_progress.config(height=4, width=20)
-        self.things_in_progress.grid(row=1, column=1)
+        self.things_in_progress.grid(row=2, column=1)
 
-        tkinter.Label(self.root, text="Any problems: ").grid(row=2, column=0)
+        tkinter.Label(self.root, text="Any problems: ").grid(row=3, column=0)
         self.configure_font(self.problems)
         self.problems.config(height=4, width=20)
-        self.problems.grid(row=2, column=1)
+        self.problems.grid(row=3, column=1)
 
     def configure_status_widgets(self):
-        self.task_list.grid(row=1, column=2, rowspan=4)
+        self.task_list.grid(row=2, column=2, rowspan=4)
         self.task_list.config(font=(self.config_manager.font_family, self.config_manager.font_size_small))
+
+    def configure_instructions(self):
+        title = tkinter.Label(self.instruction_container)
+        title.config(text="Instructions and information", font=(self.config_manager.font_family, self.config_manager.font_size_large))
+        title.grid(row=0, column=0)
+
+        instructions = tkinter.Label(self.instruction_container)
+        instructions.config(text="Application will ask you 3 question about your daily work. \nQuestions are designed so that the data you provide is suitable for reports, meetings and notes. \nApplication will save this data to a yaml file, which you can see by clicking 'Open file' button. \nYou can further edit data in this file to adapt it to your requrements.")
+        instructions.grid(row=1, column=0)
+
+        self.instruction_container.grid(row=0, column=0, columnspan=2)
+
