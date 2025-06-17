e8c98c5a9d2bd15c5b6f6b708a5f10e377383f8c
Matej2 <11059438+matej2@users.noreply.github.com>
2025-06-17 15:38:17 +0200

    Refactored main class

--- main.py
+++ main.py
@@ -16,17 +16,16 @@ from models.NoteEntry import NoteEntry
 class Application(UI):
     def __init__(self):
         self.config_manager = ConfigManager()
-        self.file_manager = FileManager(self.config_manager)
         super().__init__(self.config_manager)
 
+        self.file_manager = FileManager(self.config_manager)
         self.data_manager = DataManager(self.things_done, self.things_in_progress, self.problems, self.task_list_container, self.file_manager, self.config_manager)
         self.export_manager = ExportManager(self.config_manager, self.data_manager)
 
         self.update_time_until_next_run(datetime.now(timezone.utc) + timedelta(hours=self.config_manager.frequency_hours))
         self.scheduler = Scheduler(self.trigger_notification, self.update_time_until_next_run, self.config_manager)
 
-        self.configure_widgets()
-
+        self.configure_buttons()
         self.initialize()
 
     def initialize(self):
@@ -36,11 +35,10 @@ class Application(UI):
         today_data = self.data_manager.get_today_data_single()
         self.init_inputs(today_data)
 
-    def configure_widgets(self):
+    def configure_buttons(self):
         self.submit_button.config(command=self.on_click_submit_button)
         self.open_file.config(command=self.data_manager.file_manager.open_file)
         self.export_button.config(command=self.export_manager.export_data)
-
 
     def on_click_submit_button(self):
         self.data_manager.write_value()
@@ -50,43 +48,28 @@ class Application(UI):
         self.task_list.see(tkinter.END)
 
     def init_inputs(self, entry: NoteEntry):
-        self.fill_input_value(self.things_done, entry.things_done)
-        self.fill_input_value(self.things_in_progress, entry.to_be_done)
-        self.fill_input_value(self.problems, entry.problems)
+        self.set_text(self.things_done, entry.things_done)
+        self.set_text(self.things_in_progress, entry.to_be_done)
+        self.set_text(self.problems, entry.problems)
 
     @staticmethod
-    def set_input_disabled(text: tkinter.Text, value: str):
+    def set_text_and_disable(text: tkinter.Text, value: str):
         text.configure(state=NORMAL)
-        Application.fill_input_value(text, value)
+        Application.set_text(text, value)
         text.configure(state=DISABLED)
 
     @staticmethod
-    def fill_input_value(text: tkinter.Text, value: str):
+    def set_text(text: tkinter.Text, value: str):
         text.delete(1.0, END)
         text.insert(END, value)
 
     def update_data(self):
         today_note = self.data_manager.get_today_data()
-        self.output_task_data(today_note)
-
-    def output_task_data(self, note: NoteEntry):
-        if note.is_empty():
-            text = "No data"
-        else:
-            text = str(note)
-
-        Application.set_input_disabled(self.task_list, text)
-
-    def focus_next_widget(self, event):
-        event.widget.tk_focusNext().focus()
-        return ("break")
+        Application.set_text_and_disable(self.task_list, str(today_note))
 
     def trigger_notification(self):
         notify2.Notification("Daily notification to enter data").show()
         self.root.focus_force()
-        self.notify()
-
-    def notify(self):
         self.notification.config(text="Daily notification to enter data")
 
     def start(self):
