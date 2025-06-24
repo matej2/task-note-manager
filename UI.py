0059a73d11087e3b0291490a6c4d97b9275c835f
Matej2 <11059438+matej2@users.noreply.github.com>
2025-06-24 11:01:37 +0200

    Changed UI: Input fields and labels are in separate rows

--- UI.py
+++ UI.py
@@ -65,17 +65,17 @@ class UI(tkinter.Frame):
 
     def configure_input_widgets(self):
         tkinter.Label(self.input_container, text="What was done: ", font=self.get_font_config()).grid(row=0, column=0)
-        self.done_field.grid(row=0, column=1)
+        self.done_field.grid(row=1, column=0)
         self.done_field.config(font=self.get_font_config(), height=4, width=30, wrap=tkinter.WORD)
         self.done_field.focus_set()
 
-        tkinter.Label(self.input_container, text="What needs to be done: ", font=self.get_font_config()).grid(row=1, column=0)
+        tkinter.Label(self.input_container, text="What needs to be done: ", font=self.get_font_config()).grid(row=2, column=0)
         self.in_progress_field.config(font=self.get_font_config(), height=4, width=30, wrap=tkinter.WORD)
-        self.in_progress_field.grid(row=1, column=1)
+        self.in_progress_field.grid(row=3, column=0)
 
-        tkinter.Label(self.input_container, text="Any problems: ", font=self.get_font_config()).grid(row=2, column=0)
+        tkinter.Label(self.input_container, text="Any problems: ", font=self.get_font_config()).grid(row=4, column=0)
         self.problems_field.config(font=self.get_font_config(), height=4, width=30, wrap=tkinter.WORD)
-        self.problems_field.grid(row=2, column=1)
+        self.problems_field.grid(row=5, column=0)
 
         self.input_container.config(padx=10)
         self.input_container.grid(row=1, column=0, sticky='n', padx=10)
