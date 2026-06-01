b8873891055795afb89959ed88b04d27019bf570
Matej2 <11059438+matej2@users.noreply.github.com>
2026-06-01 12:28:53 +0200

    Updated calls to trigger notification

--- src/notification/Notification.py
+++ src/notification/Notification.py
@@ -1,5 +1,4 @@
-import notify2
-from typing_extensions import override
+from win11toast import toast, notify
 
 from src.manager.ConfigManager import ConfigManager
 from src.notification.NotificationInterface import NotificationInterface
@@ -8,14 +7,10 @@ from src.notification.NotificationInterface import NotificationInterface
 class Notification(NotificationInterface):
     def __init__(self, config_manager: ConfigManager) -> None:
         super().__init__(config_manager)
-        notify2.init("Task Note Manager")
 
     @staticmethod
     def send_info_notification(text: str):
-        notification = notify2.Notification(text)
-        notification.set_timeout(5000)
-        notification.show()
+        toast("Task Note Manager", text)
 
-    @override
     def send_notification(self) -> None:
-        notify2.Notification("Daily notification to enter data").show()
+        toast("Task Note Manager", "Daily notification to enter data", scenario='reminder')
