33d428f80d65afe0a48d0ec93941cd3fd555ac93
Matej2 <11059438+matej2@users.noreply.github.com>
2026-06-15 12:41:59 +0200

    Updated font size

--- src/manager/ConfigManager.py
+++ src/manager/ConfigManager.py
@@ -9,7 +9,7 @@ class ConfigManager:
     def __init__(self):
         # Font
         self.FONT_FAMILY = "TkDefaultFont"
-        self.REM_MULT = 13
+        self.REM_MULT = 14
         self.FONT_SIZE_LARGE = 1.3
         self.FONT_SIZE_NORMAL = 1
         self.FONT_SIZE_SMALL = 0.8
